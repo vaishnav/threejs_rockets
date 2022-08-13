76501df7cd8ac05028830b6aa595a1851aef95d7
vaishnav <vaishnav291999@gmail.com>
2022-08-14 00:34:51 +0530

    solved disallowed MIME type text html on deployment

--- rocket/vite.config.js
+++ rocket/vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: '/threejs_scene/',
+    base: '/threejs_rockets/',
     build: {
       chunkSizeWarningLimit:1500,
       rollupOptions: {
